4451d82a66075b91253c8991b14e08f931a561ad
Bruut & Braaf <info@bruutenbraaf.nl>
2019-10-03 22:31:13 +0200

    Fixes

--- header.php
+++ header.php
@@ -37,7 +37,7 @@
                                                     <?php while (have_rows('knoppen')) : the_row(); ?>
                                                         <?php $knop = get_sub_field('knop'); ?>
                                                         <?php if ($knop) { ?>
-                                                            <a class="btn<?php if (get_sub_field('is_secondair') == 1) { ?> secondair<?php } ?>" href="<?php echo $knop['url']; ?>" target="<?php echo $knop['target']; ?>"><?php echo $knop['title']; ?></a>
+                                                            <a class="btn<?php if (get_sub_field('is_secondair') == 1) { ?> secondair<?php } ?>" href="<?php echo $knop['url']; ?>" <?php if ($knop['target']) { ?>target="<?php echo $knop['target']; ?>" <?php } ?>><?php echo $knop['title']; ?></a>
                                                         <?php } ?>
                                                     <?php endwhile; ?>
                                                 </div>
@@ -131,7 +131,7 @@
                                             <?php while (have_rows('knoppen')) : the_row(); ?>
                                                 <?php $knop = get_sub_field('knop'); ?>
                                                 <?php if ($knop) { ?>
-                                                    <a class="btn<?php if (get_sub_field('is_secondair') == 1) { ?> secondair<?php } ?>" href="<?php echo $knop['url']; ?>" target="<?php echo $knop['target']; ?>"><?php echo $knop['title']; ?></a>
+                                                    <a class="btn<?php if (get_sub_field('is_secondair') == 1) { ?> secondair<?php } ?>" href="<?php echo $knop['url']; ?>" <?php if ($knop['target']) { ?>target="<?php echo $knop['target']; ?>" <?php } ?>><?php echo $knop['title']; ?></a>
                                                 <?php } ?>
                                             <?php endwhile; ?>
                                         </div>
