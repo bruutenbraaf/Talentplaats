91084821af4b438e048aa094a5fdd37904af0e9e
Bruut & Braaf <info@bruutenbraaf.nl>
2019-10-12 21:06:36 +0200

    Netwerk

---
+++ archive-ons_netwerk.php
@@ -0,0 +1,116 @@
+<?php
+get_header(); ?>
+
+<section class="header">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 d-flex align-items-center int">
+                <div class="inner">
+                    <div class="breadcrumbs">
+                        <?php if (function_exists('yoast_breadcrumb')) {
+                            yoast_breadcrumb('');
+                        } ?>
+                    </div>
+                    <?php the_field('ons_netwerk_overzicht_title', 'option'); ?>
+                    <?php the_field('ons_netwerk_overzicht_intro', 'option'); ?>
+                </div>
+            </div>
+            <div class="col-md-5 offset-md-1 d-flex align-items-center lvh nopadding">
+                <?php $achtergrond_afbeelding = get_field('ons_netwerk_overzicht_afbeelding', 'option'); ?>
+                <div class="imghdr" <?php if ($achtergrond_afbeelding) { ?> style="background-image:url(<?php echo $achtergrond_afbeelding['sizes']['home']; ?>" alt="<?php echo $achtergrond_afbeelding['alt']; ?>);" <?php } ?>>
+                </div>
+            </div>
+        </div>
+</section>
+
+<?php if (have_posts()) { ?>
+    <section class="posts-n">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 offset-md-2 text-center int-cont">
+                    <?php the_field('ons_netwerk_overzicht_titlecontent', 'option'); ?>
+                    <?php the_field('ons_netwerk_overzicht_introcontent', 'option'); ?>
+                </div>
+                <?php while (have_posts()) : the_post(); ?>
+                    <div class="col-md-6 the--post">
+                        <a href="<?php the_permalink(); ?>">
+                            <div class="in-post">
+                                <div class="postimg d-flex justify-content-end" style="background-image:url(<?php echo get_the_post_thumbnail_url($post, 'large'); ?>);">
+                                </div>
+                                <div class="inf">
+                                    <h2><?php the_title(); ?></h2>
+                                    <p><?php echo excerpt(20); ?></p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                <?php endwhile; ?>
+            </div>
+        </div>
+    </section>
+<?php } else { ?>
+    <section class="posts">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <?php _e('Geen berichten gevonden', 'talentplaats'); ?>
+                </div>
+            </div>
+        </div>
+    </section>
+<?php } ?>
+
+<section class="pagination">
+    <div class="d-flex justify-content-center align-items-center inner">
+        <?php echo paginate_links(array(
+            'prev_text' => '
+        <span class="prev">
+        <svg width="16" height="15" viewBox="0 0 16 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M16 9.49354V6.01033C16 5.52776 15.6179 5.13953 15.143 5.13953H8.00089V1.37331C8.00089 0.596842 7.07957 0.208609 6.53677 0.756489L0.251757 7.13512C-0.0839214 7.47618 -0.0839214 8.02769 0.251757 8.36875L6.53677 14.7438C7.076 15.2916 8.00089 14.9034 8.00089 14.1269V10.3643H15.143C15.6179 10.3643 16 9.97611 16 9.49354Z" fill="#00A651"/>
+        </svg>
+        </span>',
+            'next_text' => '<span class="next"> 
+            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M1 8H15M15 8L8 1M15 8L8 15" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>            
+        </span>'
+
+        )); ?>
+    </div>
+</section>
+
+<?php if (get_field('ons_netwerk_overzicht_vacatures', 'option') == 1) { ?>
+    <section class="vac-items-w">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <?php the_field('ons_netwerk_overzicht_vacturetitel', 'option'); ?>
+                </div>
+                <div class="col-md-12">
+                    <div class="arch--vac-items">
+                        <?php echo do_shortcode('[uitzendplaats_latest_vacancies]'); ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+<?php } ?>
+
+<?php if (get_field('ons_netwerk_overzicht_seo', 'option')) { ?>
+    <section class="s">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="int">
+                        <?php the_field('ons_netwerk_overzicht_seo', 'option'); ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+<?php } ?>
+
+
+
+
+<?php get_footer(); ?>
---
+++ single-ons_netwerk.php
@@ -0,0 +1,103 @@
+<?php
+get_header();
+?>
+
+<?php
+$taxonomy_prefix = 'nieuws_categorie';
+$term_id = get_queried_object()->term_id;
+$term_id_prefixed = $taxonomy_prefix . '_' . $term_id;
+$term = get_term_by('slug', get_query_var('term'), get_query_var('taxonomy'));
+?>
+
+
+<section class="relation-header">
+    <div class="container">
+        <div class="row d-flex align-items-center">
+            <div class="col-md-4 offset-md-1 comp-l">
+                <img src="<?php echo get_the_post_thumbnail_url($post, 'large'); ?>)" class="company">
+            </div>
+            <div class="offset-md-1 col-md-6">
+                <h1><?php the_title(); ?></h1>
+                <div class="breadcrumbs">
+                    <?php if (function_exists('yoast_breadcrumb')) {
+                        yoast_breadcrumb('');
+                    } ?>
+                </div>
+            </div>
+        </div>
+</section>
+<section class="sngl--content">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-1">
+            </div>
+            <div class="col-md-1 share">
+                <div class="inner">
+                    <?php if (function_exists('ADDTOANY_SHARE_SAVE_KIT')) {
+                        ADDTOANY_SHARE_SAVE_KIT(array(
+                            'buttons' => array('facebook', 'twitter'),
+                        ));
+                    } ?>
+                </div>
+            </div>
+            <div class="col-md-10 the-cnt">
+                <?php the_content(); ?>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="intressting-n">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h2><?php _e('Wellicht ook interessant?', 'talentplaats'); ?></h2>
+            </div>
+
+            <?php
+            $currentdate = date('m / d');
+            $loop = new WP_Query(array(
+                'post_type' => 'ons_netwerk',
+                'posts_per_page' => 6,
+                'order' => 'DESC'
+            )); ?>
+            <?php if ($loop->have_posts()) : ?>
+                <?php $count = $loop->post_count; ?>
+                <div class="<?php if ($count > 3) { ?>col-md-11<?php } else { ?>col-md-12<?php } ?>">
+                    <div class="post--items">
+                        <?php while ($loop->have_posts()) : $loop->the_post(); ?>
+                            <?php $countitems++; ?>
+                            <a href="<?php the_permalink(); ?>">
+                                <div class="post--item d-flex align-items-end" style="background-image:url(<?php echo get_the_post_thumbnail_url($post, 'large'); ?>);">
+                                </div>
+                            </a>
+                        <?php endwhile; ?>
+                        <?php wp_reset_postdata(); ?>
+                    </div>
+                    <script>
+                        jQuery(document).ready(function() {
+                            jQuery('.post--items').slick({
+                                slidesToShow: 3,
+                                prevArrow: jQuery('.prev'),
+                                nextArrow: jQuery('.next'),
+                            });
+                        });
+                    </script>
+                </div>
+                <?php if ($count > 3) { ?>
+                    <div class="col-md-1 d-flex align-items-center">
+                        <span class="next">
+                            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M1 8H15M15 8L8 1M15 8L8 15" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                            </svg>
+                        </span>
+                    </div>
+                <?php } ?>
+            <?php endif; ?>
+        </div>
+    </div>
+</section>
+
+
+
+<?php get_footer(); ?>
